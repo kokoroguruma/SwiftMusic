185b05040c27541d8fb221a12d7ba47a667898d0
kokoroguruma <kokorogutuma@hotmail.com>
2018-03-06 16:38:12 +0900

    クラス編成の追加を行いました。

---
+++ app/src/main/java/com/websarva/wings/android/swiftmusic/SwiftMusicApplication.java
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.swiftmusic;
+
+import android.app.Application;
+
+/**
+ * Activityで利用する際に
+ *
+ *
+ * Created by swift01 on 2018/03/06.
+ */
+
+public class SwiftMusicApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+
+}
