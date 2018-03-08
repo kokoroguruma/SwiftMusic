d2984bc32fbde9c969eb751683f99fdc9ec90e61
guiding_design <one.heart.1985.2@gmail.com>
2018-03-08 16:42:55 +0900

    全データ取得

--- app/src/main/java/com/websarva/wings/android/swiftmusic/AllActivity.java
+++ app/src/main/java/com/websarva/wings/android/swiftmusic/AllActivity.java
@@ -6,22 +6,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 
 public class AllActivity extends AppCompatActivity {
 
     public static FinishFlag mflag;
 
+    SwiftMusicApplication put;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
 
+        put = (SwiftMusicApplication) getApplication();
+
+        //全てのデータを取得
+        List<String> dateList = put.allsearch("");
+        //データが取れているかを確認
+        for (String s : dateList) {
+            System.out.println(s);
+        }
+
         ListView lvSound = (ListView)findViewById(R.id.allNameList);
 
-        lvSound.setOnItemClickListener(new allNameClick());
+        ListAdapter adapter = new ArrayAdapter<String >(this, android.R.layout.simple_list_item_1, dateList);
+
+        lvSound.setAdapter(adapter);
+
+       // lvSound.setOnItemClickListener(new allNameClick());
 
     }
 
