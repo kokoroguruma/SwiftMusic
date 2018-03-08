c5b43ab6014b309657c138fb320479fcb2c5fea4
guiding_design <one.heart.1985.2@gmail.com>
2018-03-08 11:26:41 +0900

    画面遷移完了

--- app/src/main/java/com/websarva/wings/android/swiftmusic/AllActivity.java
+++ app/src/main/java/com/websarva/wings/android/swiftmusic/AllActivity.java
@@ -1,7 +1,16 @@
 package com.websarva.wings.android.swiftmusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AllActivity extends AppCompatActivity {
 
@@ -10,6 +19,20 @@ public class AllActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
 
+        ListView lvSound = (ListView)findViewById(R.id.all_list);
 
+        lvSound.setOnItemClickListener(new allNameClick());
+
+    }
+
+    private class allNameClick implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?>parent, View view, int position, long id){
+
+            Intent intent = new Intent(AllActivity.this, PlayActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
