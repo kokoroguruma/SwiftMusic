d17a2d3bc0b7dc13183ce19135e641fd771102be
guiding_design <one.heart.1985.2@gmail.com>
2018-03-09 18:00:46 +0900

    test

--- app/src/main/java/com/websarva/wings/android/swiftmusic/PlayActivity.java
+++ app/src/main/java/com/websarva/wings/android/swiftmusic/PlayActivity.java
@@ -1,14 +1,20 @@
 package com.websarva.wings.android.swiftmusic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.media.MediaPlayer;
 
-public class PlayActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class PlayActivity extends AppCompatActivity  {
 
     private String Url;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,8 @@ public class PlayActivity extends AppCompatActivity {
         textView.setText(Url);
 
     }
+
+
 
     //戻るボタンによる画面遷移
     public void playBackClick(View view){
